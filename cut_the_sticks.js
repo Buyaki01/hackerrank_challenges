350279364bad43e47d0f2c4fbf6b89125cd97482
Buyaki <rittahbuyaki@gmail.com>
2021-02-13 22:47:28 +0300

    Add first trial of cut the sticks, not complete

---
+++ cut_the_sticks.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the cutTheSticks function below.
+function cutTheSticks(arr) {
+    var newarr = [];
+    var newarr1 = [];
+    console.log(arr.length);
+    for(var i=0; i<arr.length; i++){
+        if(arr[i]- Math.min ( ...arr ) > 1){
+            newarr.push(arr[i] - Math.min( ...arr ) );
+        }
+    }
+    console.log(newarr.length);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    let result = cutTheSticks(arr);
+
+    ws.write(result.join("\n") + "\n");
+
+    ws.end();
+}
